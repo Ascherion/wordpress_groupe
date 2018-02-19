0d847763e32d1ea74a2e8b9481c0427cd27ca88a
Ascherion <johan.desplats@gmail.com>
2018-02-19 14:17:55 +0100

    hdsockj

--- wp-content/themes/suffice/templatePerso.php
+++ wp-content/themes/suffice/templatePerso.php
@@ -3,8 +3,7 @@
  * Template Name: Custom Full Width
  * description: >-
   Page template without sidebar
- */
-/* Template Name: TemplatePerso */ ?>
+ */?>
 
 <?php get_header(); ?>
 <?php
@@ -48,5 +47,5 @@ do_action( 'suffice_before_body_content' ); ?>
  */
 do_action( 'suffice_after_body_content' ); ?>
 
-<?php //get_sidebar(); ?>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
