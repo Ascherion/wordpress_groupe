79da0b049b7968a0defe1820d1999b2de04e3d9a
LyraKuroneko <indigo.laetitia@gmail.com>
2018-02-21 10:07:20 +0100

    test tempalte

---
+++ template/html/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <?php language_attributes(); ?>
+            <meta charset="<?php bloginfo( 'charset' ); ?>">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>Meow</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <?php wp_head(); ?>
+        </head>
+
+        <body <?php body_class(); ?>>
+        </body>
