6d1a5ccea499e21f2d0c1b03f6235d75a1100904
Ascherion <johan.desplats@gmail.com>
2018-02-23 10:18:31 +0100

    AvecDesImages

--- wp-content/themes/template/index.php
+++ wp-content/themes/template/index.php
@@ -10,17 +10,17 @@ get_header();
             <div class="container-flex">
                 <div class="row">
                     <?php get_sidebar();?>
-       
+
                     <div class="col-lg-8 offset-lg-4 col-12 bgDark bgMargin">
                         <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
                             <ol class="carousel-indicators">
-                                <li data-target="#carouselIndicators" data-slide-to="0" class="active"> </li>
-                                <li data-target="#carouselIndicators" data-slide-to="1">    </li>
-                                <li data-target="#carouselIndicators" data-slide-to="2">    </li>
+                                <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselIndicators" data-slide-to="2"></li>
                             </ol>
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img class="" src="sources/images/tree.jpg" alt="First slide">
+                                    <img class="d-block w-100 carousel-image" src="<?php echo get_post_meta(70, 'crslImage', true); ?>" alt="First slide">
                                     <div class="carousel-caption d-none d-md-block">
                                         <div class="float-left">
                                             <h2>Hello</h2>
@@ -31,7 +31,7 @@ get_header();
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block w-100" src="sources/images/index.svg" alt="Second slide">
+                                    <img class="d-block w-100 carousel-image" src="<?php echo get_post_meta(70, 'crslImage1', true); ?>" alt="Second slide">
                                     <div class="carousel-caption d-none d-md-block">
                                         <div class="float-left">
                                             <h2>Hello</h2>
@@ -42,7 +42,7 @@ get_header();
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block w-100" src="sources/images/index.svg" alt="Third slide">
+                                    <img class="d-block w-100 carousel-image" src="<?php echo get_post_meta(70, 'crslImage2', true); ?>" alt="Third slide">
                                     <div class="carousel-caption d-none d-md-block">
                                         <div class="float-left">
                                             <h2>Hello</h2>
