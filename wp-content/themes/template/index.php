3fddf1b29b837bec05db369aadb66b9edda93be5
Ascherion <johan.desplats@gmail.com>
2018-02-23 09:23:39 +0100

    jojojo

--- wp-content/themes/template/index.php
+++ wp-content/themes/template/index.php
@@ -48,9 +48,9 @@ get_header();
                     <div class="col-lg-8 offset-lg-4 col-12 bgDark bgMargin">
                         <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
                             <ol class="carousel-indicators">
-                                <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselIndicators" data-slide-to="2"></li>
+                                <li data-target="#carouselIndicators" data-slide-to="0" class="active"> </li>
+                                <li data-target="#carouselIndicators" data-slide-to="1">    </li>
+                                <li data-target="#carouselIndicators" data-slide-to="2">    </li>
                             </ol>
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
