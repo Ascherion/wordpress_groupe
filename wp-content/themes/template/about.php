1e05dda213b493dd425e23757d4924da791860e0
LyraKuroneko <indigo.laetitia@gmail.com>
2018-02-23 09:22:42 +0100

    hibpi

--- wp-content/themes/template/about.php
+++ wp-content/themes/template/about.php
@@ -7,45 +7,6 @@
 */
 get_header();
 ?>
-        <div class="container-flex">
-            <div class="row">
-                <?php get_sidebar();?>
-                <div class="col-lg-8 col-12">
-                    <nav class="navbar navbar-expand navbar-dark bgDark">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 list-inline">
-                                <li class="nav-item list-inline-item">
-                                    <a class="nav-link" href="http://localhost/wordpress/index.php/Index/">Accueil</a>
-                                </li>
-                                <li class="nav-item active list-inline-item">
-                                    <a class="nav-link" href="http://localhost/wordpress/index.php/about-me/">About<span class="sr-only">(current)</span></a>
-                                </li>
-                                <li class="nav-item list-inline-item">
-                                    <a class="nav-link" href="#">Skill</a>
-                                </li>
-                                <li class="nav-item list-inline-item">
-                                    <a class="nav-link" href="#">Education</a>
-                                </li>
-                                <li class="nav-item list-inline-item">
-                                    <a class="nav-link" href="#">Experience</a>
-                                </li>
-                                <li class="nav-item list-inline-item">
-                                    <a class="nav-link" href="#">Contact</a>
-                                </li>
-                            </ul>
-                            <form class="my-2 my-lg-0">
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#searchToggle" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                                <input class="collapse form-control mr-sm-2" id="searchToggle" type="search" placeholder="Search" aria-label="Search">
-                            </form>
-                        </div>
-                    </nav>
-                </div>
-            </div>
             <div class="col-lg-8 col-12 offset-lg-4 bgDark aboutMe">
                 <h2 class="mb-5"><i class="far fa-user cyanIcon mr-2"></i>
                 <?php
